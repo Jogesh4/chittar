4642377898221841535a598f27e851433bf84714
Nikhil-Kumar-Sharma <nikhil@justconsult.us>
2022-03-31 12:32:43 +0530

    nik

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -6,14 +6,42 @@ use Illuminate\Http\Request;
 
 use App\Models\Item;
 use App\Models\Category;
+use App\Models\CartItem;
 
 class SearchController extends Controller
 {
     public function index(Request $request)
     {
-        $items = Item::where('name', 'like', $request->name.'%')->take(10)->get();
+        $category_name = "";
+        if(!empty($request->category)){
+            $category = Category::where('name', 'like', $request->category.'%')->first();
+            $category_name = $category->name;
+           $items = Item::where('category_id',$category->id)->take(10)->get();
+        }
+        else if(!empty($request->name)){
+            $items = Item::where('name', 'like', $request->name.'%')->take(10)->get();
+        }
+        else{
+            $items = Item::take(10)->get();
+        }
         $categories = Category::where('is_active', 1)->get();
+        $recent_items = Item::take(5)->get();
+        if(auth()->check()){
+        \Cart::clear();
+         \Cart::session(auth()->user()->id)->clear();
+        $carts = CartItem::where(['user_id'=>auth()->user()->id,'status' => 1])->get();
+          foreach($carts as $cart){
+            \Cart::session(auth()->user()->id)->add(array(
+                'id' => $cart->item_id,
+                'name' => "fvfdv",
+                'price' => 34,
+                'quantity' => 1,
+                'attributes' => array(),
+                'associatedModel' => array()
+            ));
+        }
+    }
 
-        return view('search.index', compact('items','categories'));
+        return view('search.index', compact('items','categories','recent_items','category_name'));
     }
 }
--- resources/views/search/index.blade.php
+++ resources/views/search/index.blade.php
@@ -14,10 +14,16 @@
 
       <div class="col-md-3">
             <div class="t-products p-2">
-                <h6 class="text-uppercase">Category</h6>
+                <h4 class="text-uppercase">Category</h4>
                 <div class="p-lists">
                    @foreach($categories as $category)
-                    <div class="d-flex justify-content-between mt-2"> <span>{{ $category->name }}</span></div>
+                    <div class="d-flex justify-content-between mt-2">
+                      @if($category_name == $category->name)
+                        <a href="/search/?category={{ $category->name }}"><span style="color:#ae0151;">{{ $category->name }}</span></a>
+                      @else
+                        <a href="/search/?category={{ $category->name }}"><span>{{ $category->name }}</span></a>
+                      @endif
+                    </div>
                   @endforeach
                 </div>
             </div>
@@ -47,11 +53,6 @@
                 
             </div>
         </div>
-
-
-
-
-
        
         <div class="col-lg-9 col-md-9 col-sm-12">
           <div class="row align-items-center g-2">
@@ -59,28 +60,46 @@
               @foreach($items as $item)
               <div class="col-sm-6 col-md-3 col-lg-3 mb-3 mb-md-0 h-100" data-aos="fade-up">
                 <div class="card card-span h-100">
-                  <img class="img-fluid h-100" src="{{ asset('storage/'.$item->image) }}" alt="..." style=" max-height: 250px !important; "/>
-                  <div class="card-img-overlay ps-0"> </div>
+                 
+                 <div style="overflow: hidden;width: 100%;position:relative;">
+                   <a class="" href="{{ route('item.show', $item) }}">
+                   <div class="img-style-low">
+                      <img class="img-fluid h-100 w-100" src="{{ asset('storage/'.$item->image) }}" alt="..." /></div ></a>
+                    <div class="fw-bold favorite-icon">
+                       @if(auth()->check())
+                                  <span class="" id="favorite_icon-{{ $item->id }}" onclick="add_favorite({{ $item->id }})"><i class="fa fa-thin fa-heart"></i></span>
+                                 <span class="favorite-active d-none" id="favorite_icon1-{{ $item->id }}"><i class="fa fa-thin fa-heart"></i></span>
+                        @else
+                                  <span class="" id="favorite_icon-{{ $item->id }}" onclick="add_favorite({{ $item->id }})"><a href="{{ route('login') }}"><i class="fa fa-thin fa-heart"></i></a></span>
+
+                        @endif
+                    </div>
+                  
+                </div> 
+                <a class="" href="{{ route('item.show', $item) }}">
                   <div class="card-body ps-0  text-center">
                     <p class="mb-0" style="text-transform: uppercase;font-size: 19px;font-weight: 500;color: #000;">{{ $item->name }}</p>
                     <div class="fw-bold"><span class="pink-color">INR {{ $item->price }}</span></div>
                   </div>
+                  </a>
+
                   <div class="d-flex"> 
+                   
                   <div class="col-8 p-2 info-pt"><i class="fas fa-cut"></i> Processing Time: 2-5 days </br> <i class="fas fa-plane"></i>  Shipping Time: 3-6 Business Days</div>
                   
                   <div class="col-2 p-2 info-pt">
-                    <input type="number" id="quantity12" name="quantity" min="1" max="12" value="1">
+                    {{-- <input type="number" id="quantity12" name="quantity" min="1" max="12" value="1"> --}}
                   </div>
                   <div class="col-2 p-2">
-                  <a class="stretched-link" href="{{ route('item.show', $item) }}"></a>
                 
                 @if(auth()->check())
                   @if(!\Cart::session(auth()->user()->id)->get($item->id))
-                  <form method="POST" action="{{ route('add.to.cart', $item) }}">
-                    @csrf
-                    <button class="bttn" style=" border: 0; background: transparent; "><i class="fas fa-cart-arrow-down" style=" color: #ae0151; font-size: 20px; "></i></button>
-                  </form>
-                  @else
+                  
+                    <button id="add-{{ $item->id }}" class="bttn" type="button" style=" border: 0; background: transparent;" onclick="add_cart({{ $item->id }})"><i class="fas fa-cart-arrow-down" style=" color: #ae0151; font-size: 20px; "></i></button>
+                    
+                    <button id="added-{{ $item->id }}" type="button" disabled class="bttn d-none" style=" border: 0; background: transparent;" ><i class="fas fa-cart-arrow-down" style=" color: #ccc; font-size: 20px; "></i></button>
+                    
+                @else
                     <button type="button" disabled class="bttn" style=" border: 0; background: transparent;" ><i class="fas fa-cart-arrow-down" style=" color: #ccc; font-size: 20px; "></i></button>
                   @endif
                 @else
@@ -92,6 +111,10 @@
                 </div>
               </div>
               @endforeach
+            @else
+                   <div class="text-center" style="margin-top: 20%;">
+                       <h3>No Product Found</h3>
+                   </div>
             @endif
           </div>
         </div>
@@ -106,33 +129,51 @@
 <div class="row m-4">
 
 <div class="col-lg-12 col-md-12 col-sm-12">
-          <div class="row align-items-center">
-            @if($items->count() > 0)
-              @foreach($items as $item)
+          <div class="row align-items-center g-2">
+            @if($recent_items->count() > 0)
+              @foreach($recent_items as $item)
               <div class="col-sm-6 col-md-3 col-lg-3 mb-3 mb-md-0 h-100" data-aos="fade-up">
                 <div class="card card-span h-100">
-                  <img class="img-fluid h-100" src="{{ asset('storage/'.$item->image) }}" alt="..." style=" max-height: 250px !important; "/>
-                  <div class="card-img-overlay ps-0"> </div>
+                 
+                 <div style="overflow: hidden;width: 100%;position:relative;">
+                   <a class="" href="{{ route('item.show', $item) }}">
+                   <div class="img-style-low">
+                      <img class="img-fluid h-100 w-100" src="{{ asset('storage/'.$item->image) }}" alt="..." /></div ></a>
+                    <div class="fw-bold favorite-icon">
+                       @if(auth()->check())
+                                  <span class="" id="recent_favorite_icon-{{ $item->id }}" onclick="add_favorite_recent({{ $item->id }})"><i class="fa fa-thin fa-heart"></i></span>
+                                 <span class="favorite-active d-none" id="recent_favorite_icon1-{{ $item->id }}"><i class="fa fa-thin fa-heart"></i></span>
+                        @else
+                                  <span class="" id="recent_favorite_icon-{{ $item->id }}" onclick="add_favorite_recent({{ $item->id }})"><a href="{{ route('login') }}"><i class="fa fa-thin fa-heart"></i></a></span>
+
+                        @endif
+                    </div>
+                  
+                </div> 
+                <a class="" href="{{ route('item.show', $item) }}">
                   <div class="card-body ps-0  text-center">
                     <p class="mb-0" style="text-transform: uppercase;font-size: 19px;font-weight: 500;color: #000;">{{ $item->name }}</p>
                     <div class="fw-bold"><span class="pink-color">INR {{ $item->price }}</span></div>
                   </div>
+                  </a>
+
                   <div class="d-flex"> 
+                   
                   <div class="col-8 p-2 info-pt"><i class="fas fa-cut"></i> Processing Time: 2-5 days </br> <i class="fas fa-plane"></i>  Shipping Time: 3-6 Business Days</div>
                   
                   <div class="col-2 p-2 info-pt">
-                    <input type="number" id="quantity12" name="quantity" min="1" max="12" value="1">
+                    {{-- <input type="number" id="quantity12" name="quantity" min="1" max="12" value="1"> --}}
                   </div>
                   <div class="col-2 p-2">
-                  <a class="stretched-link" href="{{ route('item.show', $item) }}"></a>
                 
                 @if(auth()->check())
                   @if(!\Cart::session(auth()->user()->id)->get($item->id))
-                  <form method="POST" action="{{ route('add.to.cart', $item) }}">
-                    @csrf
-                    <button class="bttn" style=" border: 0; background: transparent; "><i class="fas fa-cart-arrow-down" style=" color: #ae0151; font-size: 20px; "></i></button>
-                  </form>
-                  @else
+                  
+                    <button id="recent_add-{{ $item->id }}" class="bttn" type="button" style=" border: 0; background: transparent;" onclick="add_cart_recent({{ $item->id }})"><i class="fas fa-cart-arrow-down" style=" color: #ae0151; font-size: 20px; "></i></button>
+                    
+                    <button id="recent_added-{{ $item->id }}" type="button" disabled class="bttn d-none" style=" border: 0; background: transparent;" ><i class="fas fa-cart-arrow-down" style=" color: #ccc; font-size: 20px; "></i></button>
+                    
+                @else
                     <button type="button" disabled class="bttn" style=" border: 0; background: transparent;" ><i class="fas fa-cart-arrow-down" style=" color: #ccc; font-size: 20px; "></i></button>
                   @endif
                 @else
@@ -144,6 +185,10 @@
                 </div>
               </div>
               @endforeach
+            @else
+                   <div class="text-center" style="margin-top: 20%;">
+                       <h3>No Product Found</h3>
+                   </div>
             @endif
           </div>
         </div>
@@ -151,10 +196,114 @@
 </div>
 </section>
 
-
-
-
-
-
-
 @endsection
+
+<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+
+<script>
+
+   function add_cart(id){
+
+     var cart_no = document.getElementById('cart_no').textContent;
+
+            $.ajax({
+                    type: "Post",
+                    url: '{{route('add.to.cart')}}',
+                    datatype: 'json',
+                    data: {
+                      "_token": "{{ csrf_token() }}",
+                        'item_id' : id,
+                    },
+                    success: function (data) {
+                        const obj = JSON.parse(data);
+                         
+                           document.getElementById('add-'+id).classList.add('d-none');
+                           document.getElementById('added-'+id).classList.remove('d-none');
+                           document.getElementById('cart_no').textContent = parseInt(cart_no) + 1;
+
+                      },
+                    complete: function () {
+                    },
+                    error: function () {
+                    }
+                });
+
+    }
+
+function add_favorite(id){
+       
+            $.ajax({
+                    type: "Get",
+                    url: '{{route('add_favorite')}}',
+                    datatype: 'json',
+                    data: {
+                        'item_id' : id,
+                    },
+                    success: function (data) {
+                        const obj = JSON.parse(data);
+                         
+                           document.getElementById('favorite_icon-'+id).classList.add('d-none');
+                           document.getElementById('favorite_icon1-'+id).classList.remove('d-none');
+
+                      },
+                    complete: function () {
+                    },
+                    error: function () {
+                    }
+                });
+
+    }
+
+    function add_cart_recent(id){
+
+     var cart_no = document.getElementById('cart_no').textContent;
+
+            $.ajax({
+                    type: "Post",
+                    url: '{{route('add.to.cart')}}',
+                    datatype: 'json',
+                    data: {
+                      "_token": "{{ csrf_token() }}",
+                        'item_id' : id,
+                    },
+                    success: function (data) {
+                        const obj = JSON.parse(data);
+                         
+                           document.getElementById('recent_add-'+id).classList.add('d-none');
+                           document.getElementById('recent_added-'+id).classList.remove('d-none');
+                           document.getElementById('cart_no').textContent = parseInt(cart_no) + 1;
+
+                      },
+                    complete: function () {
+                    },
+                    error: function () {
+                    }
+                });
+
+    }
+
+function add_favorite_recent(id){
+       
+            $.ajax({
+                    type: "Get",
+                    url: '{{route('add_favorite')}}',
+                    datatype: 'json',
+                    data: {
+                        'item_id' : id,
+                    },
+                    success: function (data) {
+                        const obj = JSON.parse(data);
+                         
+                           document.getElementById('recent_favorite_icon-'+id).classList.add('d-none');
+                           document.getElementById('recent_favorite_icon1-'+id).classList.remove('d-none');
+
+                      },
+                    complete: function () {
+                    },
+                    error: function () {
+                    }
+                });
+
+    }
+
+</script>
