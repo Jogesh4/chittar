c7e37e584ac2df55fcfa8a4a5457e11b1ae9ae95
Nikhil-Kumar-Sharma <nikhil@justconsult.us>
2022-04-06 16:37:18 +0530

    nik

--- resources/views/item/show.blade.php
+++ resources/views/item/show.blade.php
@@ -13,19 +13,19 @@
               <div class="images p-3">
                 <div class="text-center p-4  ">
                    <div class="img-zoom-container mag"> 
-                     <img data-toggle="magnify" id="main-image" src="{{ asset('storage/'.$item->image) }}" width="250" />  
+                     <img data-toggle="magnify" id="main-image" src="{{ asset('storage/'.$item->image) }}" width="350" />  
                 
                 </div>
               </div>
                 <div class="thumbnail text-center">
                    @if(!empty($item->image1))
-                      <img onclick="change_image(this)" src="{{ asset('storage/'.$item->image1) }}" width="70" > 
+                      <img onclick="change_image(this)" src="{{ asset('storage/'.$item->image1) }}" width="100" > 
                    @endif
                    @if(!empty($item->image2))
-                   <img onclick="change_image(this)" src="{{ asset('storage/'.$item->image2) }}" width="70" > 
+                   <img onclick="change_image(this)" src="{{ asset('storage/'.$item->image2) }}" width="100" > 
                    @endif
                    @if(!empty($item->image3))
-                   <img onclick="change_image(this)" src="{{ asset('storage/'.$item->image3) }}" width="70" > 
+                   <img onclick="change_image(this)" src="{{ asset('storage/'.$item->image3) }}" width="100" > 
                    @endif
                 </div>
                 
@@ -369,22 +369,17 @@
 <!-- ------------------------------------------------------ -->
                 
 @endsection
-<script>
-// Initiate zoom effect:
-imageZoom("main-image", "myresult");
-</script>
+
 <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
 
-
-
-
-
-
-
-
-
-
 @section('extras')
+
+<script>
+/* Initiate Magnify Function
+with the id of the image, and the strength of the magnifier glass:*/
+magnify("main-image", 3);
+</script>
+
   <script>
 
     function edit_review(id){
@@ -442,6 +437,8 @@ imageZoom("main-image", "myresult");
     function change_image(image){
       var container = document.getElementById("main-image");
       container.src = image.src;
+      magnify("main-image", 3);
+
     }
     document.addEventListener("DOMContentLoaded", function(event) {
     });
@@ -552,46 +549,46 @@ $("#icon3").click(function(){
   </script>
 
 <script>
-function imageZoom(imgID, resultID) {
-  var img, lens, result, cx, cy;
+
+function magnify(imgID, zoom) {
+  var img, glass, w, h, bw;
   img = document.getElementById(imgID);
-  result = document.getElementById(resultID);
-  /*create lens:*/
-  lens = document.createElement("DIV");
-  lens.setAttribute("class", "img-zoom-lens");
-  /*insert lens:*/
-  img.parentElement.insertBefore(lens, img);
-  /*calculate the ratio between result DIV and lens:*/
-  cx = result.offsetWidth / lens.offsetWidth;
-  cy = result.offsetHeight / lens.offsetHeight;
-  /*set background properties for the result DIV:*/
-  result.style.backgroundImage = "url('" + img.src + "')";
-  result.style.backgroundSize = (img.width * cx) + "px " + (img.height * cy) + "px";
-  /*execute a function when someone moves the cursor over the image, or the lens:*/
-  lens.addEventListener("mousemove", moveLens);
-  img.addEventListener("mousemove", moveLens);
+  /*create magnifier glass:*/
+  glass = document.createElement("DIV");
+  glass.setAttribute("class", "img-magnifier-glass");
+  /*insert magnifier glass:*/
+  img.parentElement.insertBefore(glass, img);
+  /*set background properties for the magnifier glass:*/
+  glass.style.backgroundImage = "url('" + img.src + "')";
+  glass.style.backgroundRepeat = "no-repeat";
+  glass.style.backgroundSize = (img.width * zoom) + "px " + (img.height * zoom) + "px";
+  bw = 3;
+  w = glass.offsetWidth / 2;
+  h = glass.offsetHeight / 2;
+  /*execute a function when someone moves the magnifier glass over the image:*/
+  glass.addEventListener("mousemove", moveMagnifier);
+  img.addEventListener("mousemove", moveMagnifier);
   /*and also for touch screens:*/
-  lens.addEventListener("touchmove", moveLens);
-  img.addEventListener("touchmove", moveLens);
-  function moveLens(e) {
+  glass.addEventListener("touchmove", moveMagnifier);
+  img.addEventListener("touchmove", moveMagnifier);
+  function moveMagnifier(e) {
     var pos, x, y;
-    /*prevent any other actions that may occur when moving over the image:*/
+    /*prevent any other actions that may occur when moving over the image*/
     e.preventDefault();
     /*get the cursor's x and y positions:*/
     pos = getCursorPos(e);
-    /*calculate the position of the lens:*/
-    x = pos.x - (lens.offsetWidth / 2);
-    y = pos.y - (lens.offsetHeight / 2);
-    /*prevent the lens from being positioned outside the image:*/
-    if (x > img.width - lens.offsetWidth) {x = img.width - lens.offsetWidth;}
-    if (x < 0) {x = 0;}
-    if (y > img.height - lens.offsetHeight) {y = img.height - lens.offsetHeight;}
-    if (y < 0) {y = 0;}
-    /*set the position of the lens:*/
-    lens.style.left = x + "px";
-    lens.style.top = y + "px";
-    /*display what the lens "sees":*/
-    result.style.backgroundPosition = "-" + (x * cx) + "px -" + (y * cy) + "px";
+    x = pos.x;
+    y = pos.y;
+    /*prevent the magnifier glass from being positioned outside the image:*/
+    if (x > img.width - (w / zoom)) {x = img.width - (w / zoom);}
+    if (x < w / zoom) {x = w / zoom;}
+    if (y > img.height - (h / zoom)) {y = img.height - (h / zoom);}
+    if (y < h / zoom) {y = h / zoom;}
+    /*set the position of the magnifier glass:*/
+    glass.style.left = (x - w) + "px";
+    glass.style.top = (y - h) + "px";
+    /*display what the magnifier glass "sees":*/
+    glass.style.backgroundPosition = "-" + ((x * zoom) - w + bw) + "px -" + ((y * zoom) - h + bw) + "px";
   }
   function getCursorPos(e) {
     var a, x = 0, y = 0;
@@ -607,5 +604,6 @@ function imageZoom(imgID, resultID) {
     return {x : x, y : y};
   }
 }
+
 </script>
 @endsection
